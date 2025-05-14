9a6bebd3ef2f7b955f771754c772ba6e597ab6c1
aniciya777 <kisaost777@gmail.com>
2025-05-15 01:17:10 +0500

    Add images

--- Lesson 4/main.py
+++ Lesson 4/main.py
@@ -15,6 +15,8 @@ def main(screen: pg.Surface):
     mega_boss_images = utils.load_many_images('mega_boss.png', 2, 2, None, (100, 100))
     boss_1_images = utils.load_many_images('boss_1.png', 2, 2, None, (100, 100))
     boss_2_images = utils.load_many_images('boss_2.png', 2, 2, None, (100, 100))
+    boss_3_images = utils.load_many_images('boss_3.png', 2, 2, None, (100, 100))
+    boss_4_images = utils.load_many_images('boss_4.png', 2, 2, None, (100, 100))
     i = 0
 
     running = True
@@ -30,6 +32,8 @@ def main(screen: pg.Surface):
         screen.blit(mega_boss_images[i // 10 % 4], (100, 0))
         screen.blit(boss_1_images[i // 10 % 4], (200, 0))
         screen.blit(boss_2_images[i // 10 % 4], (300, 0))
+        screen.blit(boss_3_images[i // 10 % 4], (400, 0))
+        screen.blit(boss_4_images[i // 10 % 4], (500, 0))
 
         pg.display.flip()
         clock.tick(Config.FPS)
