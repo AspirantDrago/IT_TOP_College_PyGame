7df4cc8efa764819a81f754c9d7bf8ed43da8dd7
AspirantDrago <sanekclubstr@mail.ru>
2025-05-20 10:03:57 +0500

    move SceneManager

--- Lesson 4/main.py
+++ Lesson 4/main.py
@@ -9,41 +9,8 @@ screen = pg.display.set_mode(Config.SIZE)
 
 from src.player import Player
 from src.suhareke import Suhareke
+from src.scene import SceneManager, SkipException
 
-
-class SkipException(Exception):
-    ...
-
-
-class SceneManager:
-    def __init__(self, surface: pg.Surface, duration: float, background: pg.Color):
-        self.surface = surface
-        self.duration = duration
-        self.background = background
-        self.clock = pg.time.Clock()
-        self.group = pg.sprite.Group()
-
-    def add(self, sprite: pg.sprite.Sprite, position: tuple[int, int] = (0, 0)) -> None:
-        self.group.add(sprite)
-        sprite.rect.move_ip(position)
-
-    def __enter__(self):
-        self.time = time.time()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        while time.time() - self.time < self.duration:
-            events = pg.event.get()
-            for event in events:
-                if event.type == pg.QUIT:
-                    raise SkipException()
-                elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-                    raise SkipException()
-            self.surface.fill(self.background)
-            self.group.update()
-            self.group.draw(self.surface)
-            pg.display.flip()
-            self.clock.tick(Config.FPS)
 
 
 def main(screen: pg.Surface):
---
+++ Lesson 4/src/scene.py
@@ -0,0 +1,40 @@
+import time
+
+import pygame as pg
+
+from config import Config
+
+
+class SkipException(Exception):
+    ...
+
+
+class SceneManager:
+    def __init__(self, surface: pg.Surface, duration: float, background: pg.Color):
+        self.surface = surface
+        self.duration = duration
+        self.background = background
+        self.clock = pg.time.Clock()
+        self.group = pg.sprite.Group()
+
+    def add(self, sprite: pg.sprite.Sprite, position: tuple[int, int] = (0, 0)) -> None:
+        self.group.add(sprite)
+        sprite.rect.move_ip(position)
+
+    def __enter__(self):
+        self.time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        while time.time() - self.time < self.duration:
+            events = pg.event.get()
+            for event in events:
+                if event.type == pg.QUIT:
+                    raise SkipException()
+                elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                    raise SkipException()
+            self.surface.fill(self.background)
+            self.group.update()
+            self.group.draw(self.surface)
+            pg.display.flip()
+            self.clock.tick(Config.FPS)
